469eb3c525126519e4d2ad3cf10d3145b21e3221
김진우 <jw.kim@nhnent.com>
2018-12-10 16:28:42 +0900

    chore: version up v2.1.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,8 @@ module.exports = {
         "tui": true,
         "loadFixtures": true,
         "loadStyleFixtures": true
+    },
+    rules: {
+        indent: [2, 4, {SwitchCase: 1, ignoreComments: false, ImportDeclaration: 1, flatTernaryExpressions: false}]
     }
 };
